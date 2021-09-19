106fab0df90f245981d86663b253536f39760ca4
Yusuke Fujiki <yusuke@fujikkys.com>
2021-09-19 13:26:40 +0900

    style: fix lint errors

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,6 @@
-/* eslint-disable functional/immutable-data */
 module.exports = {
   rules: {
     "type-empty": [1, "never"],
     "subject-empty": [1, "never"],
   },
 };
-/* eslint-enable functional/immutable-data */
